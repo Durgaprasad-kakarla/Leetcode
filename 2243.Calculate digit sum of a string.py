3ba95076b5e5143e14bfe7591bcbfee2629413ad
Durgaprasad-kakarla <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2022-09-21 23:21:23 +0530

    Update 2243.Calculate digit sum of a string.py

--- 2243.Calculate digit sum of a string.py
+++ 2243.Calculate digit sum of a string.py
@@ -1,5 +1,6 @@
 class Solution:
     def digitSum(self, s: str, k: int) -> str:
+        #Here we will find sum of the digits of  a number
         def digitsum(n):
             sum1=0
             while n != 0:
@@ -8,7 +9,7 @@ class Solution:
                 n = n // 10
             return sum1
 
-
+        #here will add string and store in a string 
         def add(s, k):
             s1 = ""
             for i in range(0, len(s), k):
@@ -16,7 +17,7 @@ class Solution:
                 sum1 = digitsum(n)
                 s1 = s1 + str(sum1)
             return str(s1)
-
+        #Iterate the loop until we get len(s)<=k
         for i in range(len(s)):
             if len(s) <= k:
                 return s

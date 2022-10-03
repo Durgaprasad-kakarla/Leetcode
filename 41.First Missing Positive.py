f88bba7d94097aa2b6dea3ef9157b95314a67a3e
Durgaprasad-kakarla <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2022-10-03 22:09:31 +0530

    Create 41.First Missing Positive.py

---
+++ 41.First Missing Positive.py
@@ -0,0 +1,9 @@
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        nums.sort()
+        res = 1
+        for num in nums:
+            if num == res:
+                res += 1
+        return res
+ 

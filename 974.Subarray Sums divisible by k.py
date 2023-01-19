6d29a3a682c9596afaa4ef1766ce2f56ea01d88b
Durgaprasad-kakarla <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2023-01-19 17:11:38 +0530

    Runtime 314 ms Beats 72.97% Memory 18.9 MB Beats 63.55%

---
+++ 974.Subarray Sums divisible by k.py
@@ -0,0 +1,8 @@
+res = 0
+        prefix = 0
+        count = [1] + [0] * K
+        for a in A:
+            prefix = (prefix + a) % K
+            res += count[prefix]
+            count[prefix] += 1
+        return res

017c9a33539bcba88d79cef099be0193f6b2c735
Durgaprasad-kakarla <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2023-04-10 20:28:55 +0530

    Runtime 719ms Beats 45.1% Memory 96.3 MB Beats 29.39%

---
+++ 1020.Number of Enclaves.py
@@ -0,0 +1,36 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        def dfs(row,col,vis,board):
+            vis[row][col]=1
+            delrow=[-1,0,1,0]
+            delcol=[0,1,0,-1]
+            m=len(board[0])
+            n=len(board)
+            for i in range(4):
+                nrow=row+delrow[i]
+                ncol=col+delcol[i]
+                if nrow>=0 and nrow<n and ncol>=0 and ncol<m and not vis[nrow][ncol] and board[nrow][ncol]==1:
+                    dfs(nrow,ncol,vis,board)
+        n=len(board)
+        m=len(board[0])
+        vis=[[0 for i in range(m)]for j in range(n)]
+        #Traverse first and last row and then do dfs if we found any "O"
+        for j in range(m):
+            if not vis[0][j] and board[0][j]==1:
+                dfs(0,j,vis,board)
+            if not vis[n-1][j] and board[n-1][j]==1:
+                dfs(n-1,j,vis,board)
+        #Traverse first and last column and then do dfs if we found any "O"
+        for i in range(n):
+            if not vis[i][0] and board[i][0]==1:
+                dfs(i,0,vis,board)
+            if not vis[i][m-1] and board[i][m-1]==1:
+                dfs(i,m-1,vis,board)
+        count=0
+        for i in range(n):
+            for j in range(m):
+                if not vis[i][j] and board[i][j]==1:
+                    count+=1
+        return count
+ ''' Time Complexity--O(m*n)
+     Space Complexity--O(m*n)'''

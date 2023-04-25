9d7c55a12d7c453e85a37313e9bc5537688a3f54
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2023-04-25 17:33:22 +0530

    Runtime 46ms Beats 63.20% Memory 13.9 MB Beats 76.43%

---
+++ 38.Count and Say.py
@@ -0,0 +1,17 @@
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        s = '1'
+        for i in range(n-1):
+            let, temp, count = s[0], '', 0
+            for l in s:
+                if let == l:
+                    count += 1
+                else:
+                    temp += str(count)+let
+                    let = l
+                    count = 1
+            temp += str(count)+let
+            s = temp
+        return s
+''' Time Complexity--O(n^2)
+    Space Complexity--O(n)'''

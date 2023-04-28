339927faf39807d6d3cc60793f29a53a74f83d52
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2023-04-28 18:24:19 +0530

    Runtime 69ms Beats 7.38% Memory 20.7 MB Beats 8.42%

---
+++ 141.Linked List Cycle.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        if head is None or head.next is None:
+            return False
+        fast=head
+        slow=head
+        while fast.next and fast.next.next:
+            fast=fast.next.next
+            slow=slow.next
+            if fast==slow:
+                return True
+        return False
+''' Time Complexity--O(n)
+    Space Complexity--O(1)'''

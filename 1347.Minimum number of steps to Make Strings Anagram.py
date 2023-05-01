98291448c390a8a8a46747f0c5e0ec4179880533
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2023-05-01 19:01:37 +0530

    Runtime 139 ms Beats 69.55% Memory 16.9 MB Beats 6.52%

---
+++ 1347.Minimum number of steps to Make Strings Anagram.py
@@ -0,0 +1,17 @@
+class Solution:
+    def minSteps(self, s: str, t: str) -> int:
+        dict1=Counter(s)
+        dict2=Counter(t)
+        count=0
+        for i in dict1.keys():
+            if not dict2[i]:
+                dict2[i]=0    
+        for i in dict1.keys():
+            if dict2[i]==0:
+                count+=dict1[i]
+            else:
+                if dict1[i]>dict2[i]:
+                    count+=(dict1[i]-dict2[i])
+        return count
+''' Time Complexity--O(n)
+    Space Complexity--O(m*n)'''

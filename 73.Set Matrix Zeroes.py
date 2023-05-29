b2aa9a6554c413968bd1a2b4923193b1ea038a9a
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2023-05-29 17:43:14 +0530

    Runtime 136ms Beats 55.5% Memory 17 MB Beats 54.14%
    Time Complexity--O(m*n)+O(m*n*(m+n))
    Space  Complexity--O(max(m,n))

---
+++ 73.Set Matrix Zeroes.py
@@ -0,0 +1,20 @@
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        set1=set()
+        set2=set()
+        n=len(matrix)
+        m=len(matrix[0])
+        for i in range(n):
+            for j in range(m):
+                if matrix[i][j]==0:
+                    set1.add(i)
+                    set2.add(j)
+        for i in range(n):
+            for j in range(m):
+                if i in set1 or j in set2:
+                    matrix[i][j]=0
+''' Time Complexity--O(m*n)+O(m*n*(m+n))
+    Space Complexity--O(max(m,n))'''

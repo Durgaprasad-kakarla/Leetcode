45b6776e7811f56b5f2856a9ae72896e2af6ce92
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2023-07-19 17:08:38 +0530

    Runtime 1349 ms Beats 69.46% Memory 55.4 MB Beats 48.43%
    Time Complexity--O(n)
    Space Complexity--O(1)

---
+++ 435.Non-overlapping Intervals.py
@@ -0,0 +1,13 @@
+class Solution:
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        intervals.sort(key=lambda x: x[1])  
+        count = 0
+        end = intervals[0][1]
+        for i in range(1, len(intervals)):
+            if intervals[i][0] < end:
+                count += 1
+            else:
+                end = intervals[i][1]
+        return count
+''' Time Complexity--O(n)
+    Space Complexity--O(1)'''

c493f2869565edce39ffcdd5bacf99179edb67e8
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2023-07-25 14:09:03 +0530

    Runtime 48 ms Beats 57.19% Memory 16.5 MB Beats 70.93%
    Time Complexity--O(n)
    Space Complexity--O(1)

---
+++ 92.Reverse Linked List.py
@@ -0,0 +1,33 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseBetween(self, head: Optional[ListNode], m: int, n: int) -> Optional[ListNode]:
+        if m == n:
+            return head
+
+        dummyNode = ListNode(0)
+        dummyNode.next = head
+        pre = dummyNode
+
+        for i in range(m - 1):
+            pre = pre.next
+        
+        # reverse the [m, n] nodes
+        reverse = None
+        cur = pre.next
+        for i in range(n - m + 1):
+            next = cur.next
+            cur.next = reverse
+            reverse = cur
+            cur = next
+
+        pre.next.next = cur
+        pre.next = reverse
+
+        return dummyNode.next
+        
+''' Time Complexity--o(n)
+    Space Complexity--O(1)'''

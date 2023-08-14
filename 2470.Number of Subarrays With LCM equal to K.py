414098c9d003741210735339d7482b243c236cf2
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2023-08-14 18:23:14 +0530

    Runtime 266 ms Beats 94.92% Memory 16.4 MB Beats 62.71%
    Time Complexity--O(n*n)
    Space Complexity--O(1)

---
+++ 2470.Number of Subarrays With LCM equal to K.py
@@ -0,0 +1,14 @@
+class Solution:
+    def subarrayLCM(self, nums: List[int], k: int) -> int:
+        cnt = 0
+        for i in range(0, len(nums)):  
+            l = nums[i]            
+            for j in range(i, len(nums)):  
+                l = lcm(l,nums[j])    
+                if l == k : 
+                    cnt += 1 
+                if l > k  : 
+                    break         
+        return cnt
+''' Time Complexity--O(n*n)
+    Space Complexity--O(1)'''

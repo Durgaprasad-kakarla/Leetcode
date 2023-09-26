f8bec2b3f67fb651c8605b5b61e1046eaa508924
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2023-09-26 14:59:14 +0530

    Runtime 44 ms Beats 32.73% Memory 16.3 MB Beats 60.61%
    Time Complexity--O(n)
    Space Complexity--O(n)

---
+++ 1081. Smallest Subsequence of Distinct Characters.py
@@ -0,0 +1,36 @@
+class Solution:
+    def smallestSubsequence(self, s: str) -> str:
+        n=len(s)
+        stack=[]
+        freq=dict(Counter(s))
+        vis={i:False for i in set(s)}
+        for i in s:
+            if stack and stack[-1]>i and freq[stack[-1]]>0:
+                if vis[i]==False:
+                    while stack and stack[-1]>i and freq[stack[-1]]>0 and vis[i]==False:
+                        vis[stack[-1]]=False
+                        stack.pop(-1)
+                else:
+                    freq[i]-=1
+            if not stack:
+                stack.append(i)
+                freq[i]-=1
+                vis[i]=True
+            elif stack[-1]>i:
+                if freq[stack[-1]]==0:
+                    if vis[i]==False:
+                        stack.append(i)
+                        vis[i]=True
+                        freq[i]-=1
+                    else:
+                        freq[i]-=1
+            else:
+                if vis[i]==True:
+                    freq[i]-=1
+                else:
+                    stack.append(i)
+                    vis[i]=True
+                    freq[i]-=1
+        return "".join(stack)
+''' Time Complexity--O(n)
+    Space Complexity--O(n)'''

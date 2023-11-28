d1647b9aff6fce916330f2444a303fb025a4cc3a
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2023-11-28 19:23:56 +0530

    Runtime 389 ms Beats 67.14% of users with Python3 Memory 21.20 MB Beats 31.43% of users with Python3
    Time Complexity--O(n)
    Space Complexity--O(n)

---
+++ 2147. Number of Ways to Divide a Long Corridor.py
@@ -0,0 +1,9 @@
+class Solution:
+    def numberOfWays(self, s: str) -> int:
+        a = [i for i,c in enumerate(s) if c == 'S']
+        res = 1
+        for i in range(1,len(a) - 1,2):
+            res *= a[i+1] - a[i]
+        return res % (10**9+7) * (len(a) % 2 == 0 and len(a) >= 2)     
+''' Time Complexity--O(n)
+    Space Complexity--O(n)'''

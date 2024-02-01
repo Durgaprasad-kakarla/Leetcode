cdd9d3fb44059af0cf61926871b638347cf5b988
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2024-02-01 19:02:20 +0530

    Runtime 62 ms Beats 89.07% of users with Python3 Memory 16.61 MB Beats 64.25% of users with Python3
    Time Complexity--O(m*n)
    Space Complexity--O(m)

---
+++ 807. Max Increase to Keep City Skyline.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxIncreaseKeepingSkyline(self, grid: List[List[int]]) -> int:
+        rows_max = [0] * len(grid)
+        cols_max = [0] * len(grid[0])
+
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                rows_max[i] = max(rows_max[i], grid[i][j])
+                cols_max[j] = max(cols_max[j], grid[i][j])
+
+        res = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                res += min(rows_max[i], cols_max[j]) - grid[i][j]
+
+        return res
+''' Time Complexity--O(m*n)
+    Space Complexity--O(m)'''

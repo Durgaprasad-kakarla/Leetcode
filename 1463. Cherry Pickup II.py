8b8793c820f8e6a7240becc65e2defe1faa48852
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2024-02-13 08:46:14 +0530

    Update 1463. Cherry Pickup II.py

--- 1463. Cherry Pickup II.py
+++ 1463. Cherry Pickup II.py
@@ -11,33 +11,17 @@ class Solution:
                 return grid[i][j]+grid[i1][j1]
             if dp[i][j][i1][j1]!=-1:
                 return dp[i][j][i1][j1]
+            maxi=-float('inf')
+            for k in range(-1,2):
+                for l in range(-1,2):
+                    maxi=max(maxi,func(i+1,j+k,i1+1,j1+l,dp))
             if i==i1 and j==j1:
-                a=func(i+1,j,i1+1,j1,dp)+grid[i][j]
-                a1=func(i+1,j,i1+1,j1+1,dp)+grid[i][j]
-                a2=func(i+1,j,i1+1,j1-1,dp)+grid[i][j]
-                b=func(i+1,j+1,i1+1,j1,dp)+grid[i][j]
-                b1=func(i+1,j+1,i1+1,j1+1,dp)+grid[i][j]
-                b2=func(i+1,j+1,i1+1,j1-1,dp)+grid[i][j]
-                c=func(i+1,j-1,i1+1,j1,dp)+grid[i][j]
-                c1=func(i+1,j-1,i1+1,j1+1,dp)+grid[i][j]
-                c2=func(i+1,j-1,i1+1,j1-1,dp)+grid[i][j]
-                dp[i][j][i1][j1]=max(a,a1,a2,b,b1,b2,c,c1,c2)
-                return max(a,a1,a2,b,b1,b2,c,c1,c2)
+                maxi+=grid[i][j]
             else:
-                a=func(i+1,j,i1+1,j1,dp)+grid[i][j]+grid[i1][j1]
-                a1=func(i+1,j,i1+1,j1+1,dp)+grid[i][j]+grid[i1][j1]
-                a2=func(i+1,j,i1+1,j1-1,dp)+grid[i][j]+grid[i1][j1]
-                b=func(i+1,j+1,i1+1,j1,dp)+grid[i][j]+grid[i1][j1]
-                b1=func(i+1,j+1,i1+1,j1+1,dp)+grid[i][j]+grid[i1][j1]
-                b2=func(i+1,j+1,i1+1,j1-1,dp)+grid[i][j]+grid[i1][j1]
-                c=func(i+1,j-1,i1+1,j1,dp)+grid[i][j]+grid[i1][j1]
-                c1=func(i+1,j-1,i1+1,j1+1,dp)+grid[i][j]+grid[i1][j1]
-                c2=func(i+1,j-1,i1+1,j1-1,dp)+grid[i][j]+grid[i1][j1]
-                dp[i][j][i1][j1]=max(a,a1,a2,b,b1,b2,c,c1,c2)
-                return max(a,a1,a2,b,b1,b2,c,c1,c2)
+                maxi+=grid[i][j]+grid[i1][j1]
+            dp[i][j][i1][j1]=maxi
+            return maxi
         m=len(grid)
         n=len(grid[0])
         dp=[[[[-1 for i in range(n)]for j in range(m)]for k in range(n)]for x in range(m)]
         return func(0,0,0,len(grid[0])-1,dp)
-''' Time Complexity--O(m^2*n^2)
-    Space Complexity--O(m^2*n^2)'''

fd28ded65f5a49465a92c09de8834043a297027f
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2024-03-13 16:23:02 +0530

    Runtime 28 ms Beats 98.02% of users with Python3 Memory 16.50 MB Beats 92.38% of users with Python3
    Time Complexity--O(n)
    Space Complexity--O(1)

--- 2485.Find the Pivot Integer.py
+++ 2485.Find the Pivot Integer.py
@@ -1,9 +1,11 @@
 class Solution:
     def pivotInteger(self, n: int) -> int:
-        if n==1:
-            return 1
-        for i in range(n//2,n+1):#Starting from the middle
-            if (i*(i+1))//2 ==(n*(n+1))//2-(i*(i+1))//2+i:#Finding the sum upto i and finding the sum from i to n.Check if they are equal return i
+        pref=0
+        sm=(n*(n+1))//2
+        for i in range(1,n+1):
+            pref+=i
+            if pref==sm-pref+i:
                 return i
         return -1
-      
+''' Time Complexity--O(n)
+    Space Complexity--O(1)'''
